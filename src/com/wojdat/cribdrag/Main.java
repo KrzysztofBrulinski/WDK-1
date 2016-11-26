59e795ec401b33f6f23e4b821f579cf15d0e442a
voiddat <swir43@gmail.com>
2016-11-26 12:24:27 +0100

    fixed some minor errors

--- src/com/wojdat/cribdrag/Main.java
+++ src/com/wojdat/cribdrag/Main.java
@@ -10,24 +10,23 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
         FileReader fileReader = new FileReader();
         List<String> ciphers = fileReader.readFile("ciphers.txt");
-        StringBuilder stringBuilder = new StringBuilder("");
 
         do {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             try {
                 String s = bufferedReader.readLine();
                 String guessHex = StringUtils.utf8ToHex(s);
-                String first = ciphers.get(0);
-                String second = ciphers.get(1);
+                String first = ciphers.get(9);
+                String second = ciphers.get(10);
                 String xored = StringUtils.xor(first, second);
-                for (int i = 0; i < xored.length() - guessHex.length(); i++) {
+                for (int i = 0; i <= xored.length() - guessHex.length(); i += 2) {
                     String xorrr = StringUtils.xor(xored.substring(i, i + guessHex.length()), guessHex);
-                    String ascii = StringUtils.hexToUtf8(xorrr);
-                    byte[] xorBytes = xorrr.getBytes();
-                    System.out.println(i);
-                    System.out.println(ascii);
+                    String utf8 = StringUtils.hexToUtf8(xorrr);
+                    System.out.println(i / 2);
+                    System.out.println(utf8);
                 }
             } catch (IOException e) {
             }
