022aa9cae49325a9d9c75e447794433f1f42d825
Mateusz Wojdat <wojdatmateusz94@gmail.com>
2016-12-23 22:05:53 +0100

    polished aes

--- src/com/wojdat/aes/AesEncryptor.java
+++ src/com/wojdat/aes/AesEncryptor.java
@@ -6,14 +6,18 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
-import java.io.*;
-import java.net.URI;
+import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class AesEncryptor {
@@ -29,7 +33,7 @@ public class AesEncryptor {
         while (stringBuilder.length() < length) {
             stringBuilder.append(Integer.toHexString(r.nextInt()));
         }
-        return stringBuilder.toString().substring(0,length);
+        return stringBuilder.toString().substring(0, length);
     }
 
     public static byte[] encryptAES(byte[] key, byte[] iv, String cipherMode) {
@@ -41,10 +45,9 @@ public class AesEncryptor {
 
             Cipher cipher = Cipher.getInstance(cipherMode);
 
-            if(cipherMode.equals("AES/ECB/PKCS5Padding")) {
+            if (cipherMode.equals("AES/ECB/PKCS5Padding")) {
                 cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-            }
-            else {
+            } else {
                 cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
             }
 
@@ -55,46 +58,46 @@ public class AesEncryptor {
         return null;
     }
 
-    public static void putKeysToFile() throws IOException, URISyntaxException {
+    public static void putKeysToFile() {
         File file = new File("src/com/wojdat/aes/keys.txt");
         Path path = Paths.get("src/com/wojdat/aes/keys.txt");
-        if(!file.exists()) {
+        Charset charset = Charset.forName("UTF-8");
+        if (!file.exists()) {
             byte[] key = DatatypeConverter.parseHexBinary(getRandomHexString(64));
             byte[] iv = DatatypeConverter.parseHexBinary(getRandomHexString(32));
-            Files.write(path, key, StandardOpenOption.CREATE_NEW);
-            Files.write(path, iv, StandardOpenOption.APPEND);
-        }
-    }
+            List<String> lines = Arrays.asList(HexBin.encode(key), HexBin.encode(iv));
+            try {
+                Files.write(path, lines, charset, StandardOpenOption.CREATE_NEW);
+            } catch (IOException e) {
 
-    public static void putCiphersToFile(byte[]... ciphers) throws IOException {
-        File file = new File("src/com/wojdat/aes/ciphers.txt");
-        Path path = Paths.get("src/com/wojdat/aes/ciphers.txt");
-        if(!file.exists()) {
-            file.createNewFile();
-            for(byte[] b : ciphers) {
-                Files.write(path, b, StandardOpenOption.APPEND);
             }
         }
     }
 
-    public static byte[] getBytesFromFile(int keyOrIv) throws IOException {
-        File file = new File("src/com/wojdat/aes/keys.txt");
-        int size = keyOrIv == 0 ? 32 : 16;
-        try(FileInputStream fileInputStream = new FileInputStream(file)) {
-            byte[] result = new byte[size];
-            fileInputStream.skip(keyOrIv * 32);
-            fileInputStream.read(result);
-            return result;
+    public static void putCiphersToFile(byte[]... ciphers) {
+        Path path = Paths.get("src/com/wojdat/aes/ciphers.txt");
+        List<String> list = new ArrayList<>();
+        for (byte[] b : ciphers) {
+            list.add(HexBin.encode(b));
         }
+        try {
+            Files.write(path, list, Charset.forName("UTF-8"), StandardOpenOption.CREATE_NEW);
+        } catch (IOException e) {
+
+        }
+    }
+
+    public static byte[] getBytesFromFile(int keyOrIv) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("src/com/wojdat/aes/keys.txt"));
+        byte[] result;
+        result = DatatypeConverter.parseHexBinary(lines.get(keyOrIv));
+        return result;
     }
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         putKeysToFile();
         byte[] key = getBytesFromFile(KEY);
         byte[] iv = getBytesFromFile(IV);
-        for(byte b : iv) {
-            System.out.print(b);
-        }
         byte[] ecb = encryptAES(key, iv, "AES/ECB/PKCS5Padding");
         byte[] cbc = encryptAES(key, iv, "AES/CBC/PKCS5Padding");
         byte[] ctr = encryptAES(key, iv, "AES/CTR/NoPadding");
